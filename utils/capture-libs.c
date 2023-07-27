7322c13fdf58f38c67b49367dfd06a52e60aeee4
Simon McVittie <smcv@collabora.com>
2023-07-27 17:22:16 +0100

    capture-libs: Elevate some unexpected situations to warnings
    If one of these happens, then something odd is going on and we should
    diagnose it.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- utils/capture-libs.c
+++ utils/capture-libs.c
@@ -448,9 +448,8 @@ capture_one( const char *soname, const capture_options *options,
         {
             if( ( options->flags & CAPTURE_FLAG_IF_EXISTS ) )
             {
-                DEBUG( DEBUG_TOOL,
-                       "Unable to obtain the library %s DT_SONAME, ignoring",
-                       soname );
+                warn( "warning: Unable to obtain the library %s DT_SONAME, ignoring",
+                      soname );
                 return true;
             }
 
@@ -465,8 +464,8 @@ capture_one( const char *soname, const capture_options *options,
         {
             if( ( options->flags & CAPTURE_FLAG_IF_EXISTS ) )
             {
-                DEBUG( DEBUG_TOOL, "%s has a different DT_SONAME: %s",
-                       soname, dt_soname );
+                warn( "warning: %s has an unexpected DT_SONAME, ignoring: %s",
+                      soname, dt_soname );
                 return true;
             }
 
@@ -482,9 +481,8 @@ capture_one( const char *soname, const capture_options *options,
     {
         if( ( options->flags & CAPTURE_FLAG_IF_EXISTS ) && local_code == ENOENT )
         {
-            DEBUG( DEBUG_TOOL,
-                   "Some of the dependencies for %s have not been found, ignoring: %s",
-                   soname, local_message );
+            warn( "warning: Dependencies of %s not found, ignoring: %s",
+                  soname, local_message );
             _capsule_clear( &local_message );
             return true;
         }
