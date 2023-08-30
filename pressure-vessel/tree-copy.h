xytovl/steam-runtime-tools

commit b5441dc652fb5e7754ae7eaad13025a1e45df65e

pressure-vessel: Warn if we have to fall back to copying
pressure-vessel is designed to use hard-links when preparing the
container runtime, and we rely on them to get fast startup and not waste
disk space. If we can't link() a file to its counterpart in the
temporary runtime directory, log a warning so that people know they are
in a situation that shouldn't really happen.

To avoid spurious warnings if the copy fallback would also have failed
with a fatal error, defer the warning until after the first copy
fallback has succeeded. That way, if we can't write to the variable
data directory at all (for example EROFS or something), we won't
complain about link() failing, just show the fatal error from the copy
fallback also failing.

Helps: https://github.com/ValveSoftware/steam-runtime/issues/495
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/tree-copy.h
@@ -36,6 +36,7 @@
 typedef enum
 {
   PV_COPY_FLAGS_USRMERGE = (1 << 0),
+  PV_COPY_FLAGS_EXPECT_HARD_LINKS = (1 << 1),
   PV_COPY_FLAGS_NONE = 0
 } PvCopyFlags;
 

pressure-vessel/mtree.c
@@ -571,9 +571,11 @@ pv_mtree_apply (const char *mtree,
                 /* Or if we can copy it, that's fine too */
                 else
                   {
+                    int link_errno = errno;
+
                     g_debug ("Could not create hard link \"%s\" from \"%s/%s\" into \"%s\": %s",
                              entry.name, source_files, source, sysroot,
-                             g_strerror (errno));
+                             g_strerror (link_errno));
 
                     if (!glnx_file_copy_at (source_files_fd, source, NULL,
                                             parent_fd, base,
@@ -584,6 +586,21 @@ pv_mtree_apply (const char *mtree,
                                                 entry.name, source_files,
                                                 source, sysroot);
 
+                    if ((flags & PV_MTREE_APPLY_FLAGS_EXPECT_HARD_LINKS) != 0)
+                      {
+                        g_warning ("Unable to create hard link \"%s/%s\" to "
+                                   "\"%s/%s\": %s",
+                                   sysroot, entry.name, source_files, source,
+                                   g_strerror (link_errno));
+                        g_warning ("Falling back to copying, but this will "
+                                   "take more time and disk space.");
+                        g_warning ("For best results, \"%s\" and \"%s\" "
+                                   "should both be on the same "
+                                   "fully-featured Linux filesystem.",
+                                   source_files, sysroot);
+                        /* Only warn once per tree copied */
+                        flags &= ~PV_MTREE_APPLY_FLAGS_EXPECT_HARD_LINKS;
+                      }
                   }
               }
 

pressure-vessel/mtree.h
@@ -32,6 +32,7 @@
 typedef enum
 {
   PV_MTREE_APPLY_FLAGS_GZIP = (1 << 0),
+  PV_MTREE_APPLY_FLAGS_EXPECT_HARD_LINKS = (1 << 1),
   PV_MTREE_APPLY_FLAGS_NONE = 0
 } PvMtreeApplyFlags;
 

pressure-vessel/runtime.c
@@ -835,7 +835,8 @@ pv_runtime_create_copy (PvRuntime *self,
             }
 
           if (!pv_mtree_apply (usr_mtree, dest_usr, dest_usr_fd,
-                               self->source_files, mtree_flags,
+                               self->source_files,
+                               mtree_flags | PV_MTREE_APPLY_FLAGS_EXPECT_HARD_LINKS,
                                error))
             return FALSE;
         }
@@ -843,7 +844,7 @@ pv_runtime_create_copy (PvRuntime *self,
         {
           /* Fall back to assuming that what's on-disk is correct. */
           if (!pv_cheap_tree_copy (self->source_files, dest_usr,
-                                   PV_COPY_FLAGS_NONE, error))
+                                   PV_COPY_FLAGS_EXPECT_HARD_LINKS, error))
             return FALSE;
         }
     }

pressure-vessel/tree-copy.c
@@ -83,6 +83,7 @@ copy_tree_helper (const char *fpath,
   g_autofree gchar *target = NULL;
   GError **error = &nftw_data.error;
   gboolean usrmerge;
+  int link_errno = 0;
 
   g_return_val_if_fail (g_str_has_prefix (fpath, nftw_data.source_root), 1);
 
@@ -263,6 +264,8 @@ copy_tree_helper (const char *fpath,
         if (link (fpath, dest) == 0)
           break;
 
+        link_errno = errno;
+
         /* Slow path: fall back to copying.
          *
          * This does a FICLONE or copy_file_range to get btrfs reflinks
@@ -280,6 +283,21 @@ copy_tree_helper (const char *fpath,
             glnx_prefix_error (error, "Unable to copy \"%s\" to \"%s\"",
                                fpath, dest);
             return 1;
+          }
+
+        /* If link() failed but copying succeeded, then we might have
+         * a problem that we need to warn about. */
+        if ((nftw_data.flags & PV_COPY_FLAGS_EXPECT_HARD_LINKS) != 0)
+          {
+            g_warning ("Unable to create hard link \"%s\" to \"%s\": %s",
+                       fpath, dest, g_strerror (link_errno));
+            g_warning ("Falling back to copying, but this will take more "
+                       "time and disk space.");
+            g_warning ("For best results, \"%s\" and \"%s\" should both "
+                       "be on the same fully-featured Linux filesystem.",
+                       nftw_data.source_root, nftw_data.dest_root);
+            /* Only warn once per tree copied */
+            nftw_data.flags &= ~PV_COPY_FLAGS_EXPECT_HARD_LINKS;
           }
         break;
 

tests/pressure-vessel/cheap-copy.c
@@ -29,10 +29,16 @@
 #include "tree-copy.h"
 #include "utils.h"
 
+static gboolean opt_expect_hard_links = FALSE;
 static gboolean opt_usrmerge = FALSE;
 
 static GOptionEntry options[] =
 {
+  { "expect-hard-links", '\0',
+    G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_expect_hard_links,
+    "Show a warning if we can't use hard-links.",
+    NULL },
+
   { "usrmerge", '\0',
     G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_usrmerge,
     "Assume SOURCE is a sysroot, and carry out the /usr merge in DEST.",
@@ -76,6 +82,9 @@ main (int argc,
 
   if (opt_usrmerge)
     flags |= PV_COPY_FLAGS_USRMERGE;
+
+  if (opt_expect_hard_links)
+    flags |= PV_COPY_FLAGS_EXPECT_HARD_LINKS;
 
   if (!pv_cheap_tree_copy (argv[1], argv[2], flags, error))
     goto out;

commit 9d7e306284d67d27a66d7362388ab787581f0439

pv-tree-copy: Document EXPECT_HARD_LINKS flag
Fixes: b5441dc6 "pressure-vessel: Warn if we have to fall back to copying"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/tree-copy.h
@@ -29,6 +29,8 @@
  * @PV_COPY_FLAGS_USRMERGE: Transform the copied tree by merging
  *  /bin, /sbin, /lib* into /usr, and replacing them with symbolic
  *  links /bin -> usr/bin and so on.
+ * @PV_COPY_FLAGS_EXPECT_HARD_LINKS: Log a warning if unable to create
+ *  hard links between @source_root and @dest_root.
  * @PV_RESOLVE_FLAGS_NONE: No special behaviour.
  *
  * Flags affecting how pv_cheap_tree_copy() behaves.