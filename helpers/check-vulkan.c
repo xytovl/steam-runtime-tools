xytovl/steam-runtime-tools

commit 44df5dfafa188e8c7ab985825b9cacaaf4bbe114

helpers: Warn if closing stdout fails
Signed-off-by: Simon McVittie <smcv@collabora.com>

helpers/check-vulkan.c
@@ -1497,6 +1497,9 @@ int main (int argc,
         ret = EXIT_SUCCESS;
     }
 
+  if (fclose (original_stdout) != 0)
+    g_warning ("Unable to close stdout: %s", g_strerror (errno));
+
 out:
   if (local_error != NULL)
     g_printerr ("%s", local_error->message);

helpers/check-xdg-portal.c
@@ -248,5 +248,8 @@ main (int argc,
                                 SRT_JSON_OUTPUT_FLAGS_PRETTY, &local_error))
     g_warning ("%s", local_error->message);
 
+  if (fclose (original_stdout) != 0)
+    g_warning ("Unable to close stdout: %s", g_strerror (errno));
+
   return ret;
 }

commit 6ced7c9db80b1baadd967e86f19b6638597eccb9

helpers: Fix use-after-free closing standard output
In these two helpers, original_stdout is automatically freed on exit
from its scope, so we must not close it in a way that leaves a dangling
pointer to the freed object.

Fixes: 44df5dfa "helpers: Warn if closing stdout fails"
Signed-off-by: Simon McVittie <smcv@collabora.com>

helpers/check-vulkan.c
@@ -1497,7 +1497,7 @@ int main (int argc,
         ret = EXIT_SUCCESS;
     }
 
-  if (fclose (original_stdout) != 0)
+  if (fclose (g_steal_pointer (&original_stdout)) != 0)
     g_warning ("Unable to close stdout: %s", g_strerror (errno));
 
 out:

helpers/check-xdg-portal.c
@@ -248,7 +248,7 @@ main (int argc,
                                 SRT_JSON_OUTPUT_FLAGS_PRETTY, &local_error))
     g_warning ("%s", local_error->message);
 
-  if (fclose (original_stdout) != 0)
+  if (fclose (g_steal_pointer (&original_stdout)) != 0)
     g_warning ("Unable to close stdout: %s", g_strerror (errno));
 
   return ret;