xytovl/steam-runtime-tools

commit 52e494e5ec1a18fb7d963253bbfbb7219f225c23

utils: Unignore all signals, not just the ones Steam historically altered
To get from an unknown execution environment into a known state, it's
more robust if we reset all signals, not just a few.

Signed-off-by: Simon McVittie <smcv@collabora.com>

steam-runtime-tools/utils.c
@@ -631,20 +631,12 @@ _srt_constructor (void)
   g_return_if_fail (_srt_check_not_setuid ());
 }
 
-static const int signals_blocked_by_steam[] =
-  {
-    SIGALRM,
-    SIGCHLD,
-    SIGPIPE,
-    SIGTRAP
-  };
-
 /*
  * _srt_child_setup_unblock_signals:
  * @ignored: Ignored, for compatibility with #GSpawnChildSetupFunc
  *
- * A child-setup function that unblocks all signals, and resets signals
- * known to be altered by the Steam client to their default dispositions.
+ * A child-setup function that unblocks all signals, and resets all signals
+ * to their default dispositions.
  *
  * In particular, this can be used to work around versions of `timeout(1)`
  * that do not do configure `SIGCHLD` to make sure they receive it
@@ -657,22 +649,24 @@ _srt_child_setup_unblock_signals (gpointer ignored)
 {
   struct sigaction action = { .sa_handler = SIG_DFL };
   sigset_t new_set;
-  gsize i;
+  int sig;
 
   /* We ignore errors and don't even g_debug(), to avoid being
    * async-signal-unsafe */
   sigemptyset (&new_set);
   (void) pthread_sigmask (SIG_SETMASK, &new_set, NULL);
 
-  for (i = 0; i < G_N_ELEMENTS (signals_blocked_by_steam); i++)
-    (void) sigaction (signals_blocked_by_steam[i], &action, NULL);
+  for (sig = 1; sig < NSIG; sig++)
+    {
+      if (sig != SIGKILL && sig != SIGSTOP)
+        (void) sigaction (sig, &action, NULL);
+    }
 }
 
 /*
  * _srt_unblock_signals:
  *
- * Unblock all signals, and reset signals known to be altered by the
- * Steam client to their default dispositions.
+ * Unblock all signals, and reset all signals to their default dispositions.
  *
  * This function is not async-signal-safe.
  *
@@ -690,7 +684,6 @@ _srt_unblock_signals (void)
   struct sigaction new_action = { .sa_handler = SIG_DFL };
   sigset_t old_set;
   sigset_t new_set;
-  gsize i;
   int sig;
   int saved_errno;
 
@@ -706,7 +699,7 @@ _srt_unblock_signals (void)
     }
   else
     {
-      for (sig = 1; sig < 64; sig++)
+      for (sig = 1; sig < NSIG; sig++)
         {
           /* sigismember returns -1 for non-signals, which we ignore */
           if (sigismember (&new_set, sig) == 1 &&
@@ -715,15 +708,19 @@ _srt_unblock_signals (void)
         }
     }
 
-  for (i = 0; i < G_N_ELEMENTS (signals_blocked_by_steam); i++)
+  for (sig = 1; sig < NSIG; sig++)
     {
-      sig = signals_blocked_by_steam[i];
+      if (sig == SIGKILL || sig == SIGSTOP)
+        continue;
 
       if (sigaction (sig, &new_action, &old_action) != 0)
         {
           saved_errno = errno;
-          g_warning ("Unable to reset handler for signal %d (%s): %s",
-                     sig, g_strsignal (sig), g_strerror (saved_errno));
+
+          /* EINVAL is returned for the signals reserved by glibc */
+          if (saved_errno != EINVAL)
+            g_warning ("Unable to reset handler for signal %d (%s): %s",
+                       sig, g_strsignal (sig), g_strerror (saved_errno));
         }
       else if (old_action.sa_handler == SIG_IGN)
         {

commit 6b15bf28dffc373435d6f442d6d095a7ff59c0eb

utils: Don't reset SIGBUS, SIGFPE, SIGSEGV handlers under AddressSanitizer
AddressSanitizer sets handlers for these signals to provide better
diagnostics.

Fixes: 52e494e5 "utils: Unignore all signals, not just the ones Steam historically altered"
Signed-off-by: Simon McVittie <smcv@collabora.com>

steam-runtime-tools/utils-internal.h
@@ -41,6 +41,12 @@
 #include <steam-runtime-tools/macros.h>
 #include <steam-runtime-tools/glib-backports-internal.h>
 
+#ifdef __has_feature
+#define _srt_compiler_has_feature __has_feature
+#else
+#define _srt_compiler_has_feature(x) (0)
+#endif
+
 typedef enum
 {
   SRT_HELPER_FLAGS_SEARCH_PATH = (1 << 0),

steam-runtime-tools/utils.c
@@ -713,6 +713,15 @@ _srt_unblock_signals (void)
       if (sig == SIGKILL || sig == SIGSTOP)
         continue;
 
+#if defined(__SANITIZE_ADDRESS__) || _srt_compiler_has_feature(address_sanitizer)
+      /* AddressSanitizer sets handlers for these signals during startup.
+       * Leave those in place, and don't show a warning for them. */
+      if ((sig == SIGBUS || sig == SIGFPE || sig == SIGSEGV)
+          && sigaction (sig, NULL, &old_action) == 0
+          && old_action.sa_handler != SIG_IGN)
+        continue;
+#endif
+
       if (sigaction (sig, &new_action, &old_action) != 0)
         {
           saved_errno = errno;