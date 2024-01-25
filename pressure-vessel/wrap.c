xytovl/steam-runtime-tools

commit 8ee66f4cf8b0f2a511030a92e01367643e7e22c5

pv-wrap: Don't register stdout, stderr, --pass-fd with FlatpakBwrap
This fixes two separate issues.

If we add a file descriptor to the FlatpakBwrap object with
flatpak_bwrap_add_fd, then flatpak_bwrap_child_setup() will lseek()
back to byte 0 before it execs the replacement process. This is
often what we want - for example, when we have written out a file
containing the container identification, we want any subsequent
read from that file to start at byte 0.

However, it is absolutely not what we want if stdout/stderr are directed
to a log file, in which case any writes after the lseek() will start from
the beginning and overwrite the content of the log file. The symptom for
this is that debug messages from pv-adverb start at the beginning of the
log file, overwriting earlier debug messages from pv-wrap (but directing
the log to a pipe or to the systemd Journal works fine, because those
aren't seekable). Similarly, if --pass-fd is being used for a seekable
file, we probably don't want to force it to seek back to byte 0.

The second issue is that adding a fd with flatpak_bwrap_add_fd()
means the FlatpakBwrap object takes responsibility for closing that
fd. This was incorrect to do for original_stdout and original_stderr
without transferring ownership (which we don't want to do), because
they are declared with glnx_autofd, meaning that they will be closed
automatically when they go out of scope. In the error code path where
pv_bwrap_execve() fails, this means they will be closed twice - once
by glnx_autofd, and once by the g_autoptr(FlatpakBwrap) - leading to
an assertion failure. This is mitigated by not being a problem in the
success code path where pv_bwrap_execve() succeeds, which in practice
is the one we always want to reach.

Avoid both of these by using a separate list of fds to inherit, not
seeking back to the beginning of those, and not closing them.

This has the side-effect that fds from --pass-fd are also not closed,

pressure-vessel/wrap.c
@@ -936,6 +936,7 @@ int
 main (int argc,
       char *argv[])
 {
+  g_autoptr(GArray) inherit_fds = g_array_new (FALSE, FALSE, sizeof (int));
   g_autoptr(GOptionContext) context = NULL;
   g_autoptr(GError) local_error = NULL;
   GError **error = &local_error;
@@ -2258,10 +2259,10 @@ main (int argc,
                               "--subreaper",
                               NULL);
 
-      flatpak_bwrap_add_fd (adverb_argv, original_stdout);
+      g_array_append_val (inherit_fds, original_stdout);
       flatpak_bwrap_add_arg_printf (adverb_argv, "--assign-fd=%d=%d",
                                     STDOUT_FILENO, original_stdout);
-      flatpak_bwrap_add_fd (adverb_argv, original_stderr);
+      g_array_append_val (inherit_fds, original_stderr);
       flatpak_bwrap_add_arg_printf (adverb_argv, "--assign-fd=%d=%d",
                                     STDERR_FILENO, original_stderr);
 
@@ -2271,7 +2272,7 @@ main (int argc,
             {
               int fd = g_array_index (opt_pass_fds, int, i);
 
-              flatpak_bwrap_add_fd (adverb_argv, fd);
+              g_array_append_val (inherit_fds, fd);
               flatpak_bwrap_add_arg_printf (adverb_argv, "--pass-fd=%d", fd);
             }
         }
@@ -2469,7 +2470,9 @@ main (int argc,
   if (opt_systemd_scope)
     pv_wrap_move_into_scope (steam_app_id);
 
-  pv_bwrap_execve (final_argv, error);
+  pv_bwrap_execve (final_argv,
+                   (int *) inherit_fds->data, inherit_fds->len,
+                   error);
 
 out:
   if (local_error != NULL)

pressure-vessel/bwrap.c
@@ -60,6 +60,9 @@ pv_bwrap_run_sync (FlatpakBwrap *bwrap,
 /**
  * pv_bwrap_execve:
  * @bwrap: A #FlatpakBwrap on which flatpak_bwrap_finish() has been called
+ * @inherit_fds (array length=n_inherit_fds): Allow these fds to be
+ *  inherited across execve(), but without seeking to the beginning
+ * @n_inherit_fds: Number of entries in @inherit_fds
  * @error: Used to raise an error on failure
  *
  * Attempt to replace the current process with the given bwrap command.
@@ -69,9 +72,12 @@ pv_bwrap_run_sync (FlatpakBwrap *bwrap,
  */
 gboolean
 pv_bwrap_execve (FlatpakBwrap *bwrap,
+                 int *inherit_fds,
+                 size_t n_inherit_fds,
                  GError **error)
 {
   int saved_errno;
+  size_t i;
 
   g_return_val_if_fail (bwrap != NULL, FALSE);
   g_return_val_if_fail (bwrap->argv->len >= 2, FALSE);
@@ -83,6 +89,20 @@ pv_bwrap_execve (FlatpakBwrap *bwrap,
 
   if (bwrap->fds != NULL && bwrap->fds->len > 0)
     flatpak_bwrap_child_setup_cb (bwrap->fds);
+
+  for (i = 0; i < n_inherit_fds; i++)
+    {
+      int fd = inherit_fds[i];
+      int fd_flags;
+
+      fd_flags = fcntl (fd, F_GETFD);
+
+      if (fd_flags >= 0
+          && (fd_flags & FD_CLOEXEC) != 0
+          && fcntl (fd, F_SETFD, fd_flags & ~FD_CLOEXEC) != 0)
+        g_warning ("Unable to clear close-on-exec flag of fd %d: %s",
+                   fd, g_strerror (errno));
+    }
 
   fflush (stdout);
   fflush (stderr);

pressure-vessel/bwrap.h
@@ -41,6 +41,8 @@ gboolean pv_bwrap_run_sync (FlatpakBwrap *bwrap,
                             int *exit_status_out,
                             GError **error);
 gboolean pv_bwrap_execve (FlatpakBwrap *bwrap,
+                          int *inherit_fds,
+                          size_t n_inherit_fds,
                           GError **error);
 gboolean pv_bwrap_bind_usr (FlatpakBwrap *bwrap,
                             const char *provider_in_host_namespace,

commit 9a5be0bea55293ab16314df7ff6a07b049d978f4

pv-wrap: When running in Flatpak, forward inherited fds to subsandbox
When we are using steam-runtime-launch-client to set up a subsandbox
for the pressure-vessel container, it is not enough to mark fds as
non-O_CLOEXEC: we also need to instruct s-r-launch-client to forward them
over D-Bus to flatpak-portal. We already did this for the fds managed
by the FlatpakBwrap object, but not for other inherited fds.

Before v0.20240103.0, all fds that we wanted to send to the remote
subsandbox were managed by the FlatpakBwrap object, but this was wrong
because it had the side-effect of seeking log files back to byte 0
(steamrt/tasks#370). Since v0.20240103.0, we split out some of the fds
into the inherit_fds array, but we did not emit --forward-fd for those.

In some situations (notably launching the SteamLinuxRuntime_sniper
container for the steamwebhelper process in recent Steam client betas)
this worked anyway, but it seems that this only happened by coincidence.

Addresses <https://github.com/ValveSoftware/steam-runtime/issues/644>
and probably <https://github.com/flathub/com.valvesoftware.Steam/issues/1235>.

Fixes: 8ee66f4c "pv-wrap: Don't register stdout, stderr, --pass-fd with FlatpakBwrap"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap.c
@@ -2371,6 +2371,13 @@ main (int argc,
           flatpak_bwrap_add_arg (flatpak_subsandbox, fd_str);
         }
 
+      for (i = 0; i < inherit_fds->len; i++)
+        {
+          g_autofree char *fd_str = g_strdup_printf ("--forward-fd=%d",
+                                                     g_array_index (inherit_fds, int, i));
+          flatpak_bwrap_add_arg (flatpak_subsandbox, fd_str);
+        }
+
       flatpak_bwrap_add_arg (flatpak_subsandbox, "--");
 
       g_warn_if_fail (g_strv_length (flatpak_subsandbox->envp) == 0);