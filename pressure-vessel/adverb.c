xytovl/steam-runtime-tools

commit 4329cec7b92e1bf4753bb8e843ac50140a746293

pv-adverb: Add --assign-fd
We'll use this in pv-wrap in a subsequent commit.

Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/adverb.c
@@ -53,6 +53,7 @@
 static const char * const *global_original_environ = NULL;
 static GPtrArray *global_locks = NULL;
 static GPtrArray *global_ld_so_conf_entries = NULL;
+static GArray *global_assign_fds = NULL;
 static GArray *global_pass_fds = NULL;
 static gboolean opt_batch = FALSE;
 static gboolean opt_create = FALSE;
@@ -73,8 +74,14 @@ static GPid child_pid;
 
 typedef struct
 {
-  int original_stdout_fd;
-  int original_stderr_fd;
+  int target;
+  int source;
+} AssignFd;
+
+typedef struct
+{
+  size_t n_assign_fds;
+  const AssignFd *assign_fds;
   size_t n_pass_fds;
   const int *pass_fds;
 } ChildSetupData;
@@ -115,17 +122,6 @@ child_setup_cb (gpointer user_data)
         signal (i, SIG_DFL);
     }
 
-  /* Put back the original stdout for the child process */
-  if (data != NULL &&
-      data->original_stdout_fd > 0 &&
-      dup2 (data->original_stdout_fd, STDOUT_FILENO) != STDOUT_FILENO)
-    _srt_async_signal_safe_error ("pressure-vessel-adverb: Unable to reinstate original stdout\n", LAUNCH_EX_FAILED);
-
-  if (data != NULL &&
-      data->original_stderr_fd > 0 &&
-      dup2 (data->original_stderr_fd, STDERR_FILENO) != STDERR_FILENO)
-    _srt_async_signal_safe_error ("pressure-vessel-adverb: Unable to reinstate original stderr\n", LAUNCH_EX_FAILED);
-
   /* Make the fds we pass through *not* be close-on-exec */
   if (data != NULL)
     {
@@ -147,6 +143,15 @@ child_setup_cb (gpointer user_data)
               && fcntl (fd, F_SETFD, fd_flags & ~FD_CLOEXEC) != 0)
             _srt_async_signal_safe_error ("pressure-vessel-adverb: Unable to clear close-on-exec\n",
                                           LAUNCH_EX_FAILED);
+        }
+
+      for (j = 0; j < data->n_assign_fds; j++)
+        {
+          int target = data->assign_fds[j].target;
+          int source = data->assign_fds[j].source;
+
+          if (dup2 (source, target) != target)
+            _srt_async_signal_safe_error ("pressure-vessel-adverb: Unable to assign file descriptors\n", LAUNCH_EX_FAILED);
         }
     }
 }
@@ -292,6 +297,54 @@ opt_ld_preload_cb (const gchar *option_name,
 {
   return opt_ld_something (option_name, PV_PRELOAD_VARIABLE_INDEX_LD_PRELOAD,
                            value, data, error);
+}
+
+static gboolean
+opt_assign_fd_cb (const char *name,
+                  const char *value,
+                  gpointer data,
+                  GError **error)
+{
+  char *endptr;
+  gint64 i64 = g_ascii_strtoll (value, &endptr, 10);
+  AssignFd pair;
+  int fd_flags;
+
+  g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
+  g_return_val_if_fail (value != NULL, FALSE);
+  g_return_val_if_fail (global_assign_fds != NULL, FALSE);
+
+  if (i64 < 0 || i64 > G_MAXINT || endptr == value || *endptr != '=')
+    {
+      g_set_error (error, G_OPTION_ERROR, G_OPTION_ERROR_BAD_VALUE,
+                   "Target fd out of range or invalid: %s", value);
+      return FALSE;
+    }
+
+  pair.target = (int) i64;
+  fd_flags = fcntl (pair.target, F_GETFD);
+
+  if (fd_flags < 0)
+    return glnx_throw_errno_prefix (error, "Unable to receive --assign-fd target %d", pair.target);
+
+  value = endptr + 1;
+  i64 = g_ascii_strtoll (value, &endptr, 10);
+
+  if (i64 < 0 || i64 > G_MAXINT || endptr == value || *endptr != '\0')
+    {
+      g_set_error (error, G_OPTION_ERROR, G_OPTION_ERROR_BAD_VALUE,
+                   "Source fd out of range or invalid: %s", value);
+      return FALSE;
+    }
+
+  pair.source = (int) i64;
+  fd_flags = fcntl (pair.source, F_GETFD);
+
+  if (fd_flags < 0)
+    return glnx_throw_errno_prefix (error, "Unable to receive --assign-fd source %d", pair.source);
+
+  g_array_append_val (global_assign_fds, pair);
+  return TRUE;
 }
 
 static gboolean
@@ -761,6 +814,11 @@ terminate_child_cb (int signum)
 
 static GOptionEntry options[] =
 {
+  { "assign-fd", '\0',
+    G_OPTION_FLAG_NONE, G_OPTION_ARG_CALLBACK, opt_assign_fd_cb,
+    "Make fd TARGET a copy of SOURCE, like TARGET>&SOURCE in shell.",
+    "TARGET=SOURCE" },
+
   { "batch", '\0',
     G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_batch,
     "Disable all interactivity and redirection: ignore --shell*, "
@@ -911,6 +969,7 @@ main (int argc,
   g_auto(GStrv) original_environ = NULL;
   g_autoptr(GPtrArray) ld_so_conf_entries = NULL;
   g_autoptr(GPtrArray) locks = NULL;
+  g_autoptr(GArray) assign_fds = NULL;
   g_autoptr(GArray) pass_fds = NULL;
   g_autoptr(GOptionContext) context = NULL;
   g_autoptr(GError) local_error = NULL;
@@ -920,11 +979,12 @@ main (int argc,
   g_autofree gchar *locales_temp_dir = NULL;
   glnx_autofd int original_stdout = -1;
   glnx_autofd int original_stderr = -1;
-  ChildSetupData child_setup_data = { -1, -1, 0, NULL };
+  ChildSetupData child_setup_data = { 0, NULL, 0, NULL };
   sigset_t mask;
   struct sigaction terminate_child_action = {};
   g_autoptr(FlatpakBwrap) wrapped_command = NULL;
   g_autoptr(PvPerArchDirs) lib_temp_dirs = NULL;
+  AssignFd pair;
   gsize i;
 
   sigemptyset (&mask);
@@ -952,6 +1012,9 @@ main (int argc,
   pass_fds = g_array_new (FALSE, FALSE, sizeof (int));
   global_pass_fds = pass_fds;
 
+  assign_fds = g_array_new (FALSE, FALSE, sizeof (AssignFd));
+  global_assign_fds = assign_fds;
+
   /* Set up the initial base logging */
   if (!_srt_util_set_glib_log_handler ("pressure-vessel-adverb",
                                        G_LOG_DOMAIN, SRT_LOG_FLAGS_NONE,
@@ -961,6 +1024,15 @@ main (int argc,
       ret = EX_UNAVAILABLE;
       goto out;
     }
+
+  /* Before parsing arguments, the default is like shell redirection
+   * 1>&original_stdout 2>&original_stderr */
+  pair.target = STDOUT_FILENO;
+  pair.source = glnx_steal_fd (&original_stdout);
+  g_array_append_val (assign_fds, pair);
+  pair.target = STDERR_FILENO;
+  pair.source = glnx_steal_fd (&original_stderr);
+  g_array_append_val (assign_fds, pair);
 
   context = g_option_context_new (
       "COMMAND [ARG...]\n"
@@ -1177,8 +1249,8 @@ main (int argc,
   sigaction (SIGUSR2, &terminate_child_action, NULL);
 
   g_debug ("Launching child process...");
-  child_setup_data.original_stdout_fd = original_stdout;
-  child_setup_data.original_stderr_fd = original_stderr;
+  child_setup_data.assign_fds = (const AssignFd *) assign_fds->data;
+  child_setup_data.n_assign_fds = assign_fds->len;
   child_setup_data.pass_fds = (const int *) pass_fds->data;
   child_setup_data.n_pass_fds = pass_fds->len;
 
@@ -1205,6 +1277,20 @@ main (int argc,
           quoted = g_shell_quote (wrapped_command->envp[i]);
           g_message ("\t%s", quoted);
         }
+
+      g_message ("Inherited file descriptors:");
+
+      for (i = 0; i < pass_fds->len; i++)
+        g_message ("\t%d", g_array_index (pass_fds, int, i));
+
+      g_message ("Redirections:");
+
+      for (i = 0; i < assign_fds->len; i++)
+        {
+          const AssignFd *item = &g_array_index (assign_fds, AssignFd, i);
+
+          g_message ("\t%d>&%d", item->target, item->source);
+        }
     }
 
   fflush (stdout);
@@ -1228,9 +1314,9 @@ main (int argc,
 
   /* If the parent or child writes to a passed fd and closes it,
    * don't stand in the way of that. Skip fds 0 (stdin),
-   * 1 (stdout) and 2 (stderr); we have moved our original stdout
-   * to another fd which will be dealt with below, and we want to keep
-   * our stdin and stderr open. */
+   * 1 (stdout) and 2 (stderr); we have moved our original stdout/stderr
+   * to another fd, which will be dealt with as one of the assign_fds,
+   * and we want to keep our current stdin, stdout and stderr open. */
   for (i = 0; i < pass_fds->len; i++)
     {
       int fd = g_array_index (pass_fds, int, i);
@@ -1239,9 +1325,15 @@ main (int argc,
         close (fd);
     }
 
-  /* If the child writes to stdout and closes it, don't interfere */
-  glnx_close_fd (&original_stdout);
-  glnx_close_fd (&original_stderr);
+  /* Same for reassigned fds, notably our original stdout and stderr */
+  for (i = 0; i < assign_fds->len; i++)
+    {
+      const AssignFd *item = &g_array_index (assign_fds, AssignFd, i);
+      int fd = item->source;
+
+      if (fd > 2)
+        close (fd);
+    }
 
   /* Reap child processes until child_pid exits */
   if (!pv_wait_for_child_processes (child_pid, &wait_status, error))
@@ -1287,6 +1379,7 @@ main (int argc,
     }
 
 out:
+  global_assign_fds = NULL;
   global_ld_so_conf_entries = NULL;
   global_locks = NULL;
   global_pass_fds = NULL;

commit 864ad2b51c8ba358f70ad1802bedaa392b2bbb70

pv-adverb: Don't require target of --assign-fd to be a valid fd yet
If we're doing `pv-adverb --assign-fd=9=1` (the equivalent of `9>&1`
in the shell), then fd 1 needs to be a valid fd already, but fd 9
does not.

This was not a problem in practice because pressure-vessel currently
only assigns to fds 1 and 2 (stdout and stderr), which we ensure are
always valid, even if that means opening /dev/null.

Fixes: 4329cec7 "pv-adverb: Add --assign-fd"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/adverb.c
@@ -321,11 +321,10 @@ opt_assign_fd_cb (const char *name,
       return FALSE;
     }
 
+  /* Note that the target does not need to be a valid fd yet -
+   * we can use something like --assign-fd=9=1 to make fd 9
+   * a copy of existing fd 1 */
   pair.target = (int) i64;
-  fd_flags = fcntl (pair.target, F_GETFD);
-
-  if (fd_flags < 0)
-    return glnx_throw_errno_prefix (error, "Unable to receive --assign-fd target %d", pair.target);
 
   value = endptr + 1;
   i64 = g_ascii_strtoll (value, &endptr, 10);