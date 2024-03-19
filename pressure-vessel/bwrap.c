xytovl/steam-runtime-tools

commit 3fbb5d8a8b732aa2cc3c1e1725ed0611213280fc

Rename pv-launch to s-r-launch-client and move it to bin/
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap.c
@@ -1933,7 +1933,7 @@ main (int argc,
       else
         {
           g_assert (flatpak_subsandbox != NULL);
-          /* pressure-vessel-launch currently hard-codes this */
+          /* steam-runtime-launch-client currently hard-codes this */
           g_warning ("Process ID namespace is always shared when using a "
                      "Flatpak subsandbox");
         }

bin/launch-client.c
@@ -1,5 +1,5 @@
 /*
- * pressure-vessel-launch — send IPC requests to create child processes
+ * steam-runtime-launch-client — send IPC requests to create child processes
  *
  * Copyright © 2018 Red Hat, Inc.
  * Copyright © 2020-2021 Collabora Ltd.
@@ -46,7 +46,6 @@
 
 #include "flatpak-portal.h"
 #include "flatpak-session-helper.h"
-#include "utils.h"
 
 typedef enum {
   FLATPAK_HOST_COMMAND_FLAGS_CLEAR_ENV = 1 << 0,
@@ -738,14 +737,14 @@ main (int argc,
   original_environ = g_get_environ ();
   global_original_environ = (const char * const *) original_environ;
 
-  g_set_prgname ("pressure-vessel-launch");
+  g_set_prgname ("steam-runtime-launch-client");
 
   /* Set up the initial base logging */
   _srt_util_set_glib_log_handler (FALSE);
 
   context = g_option_context_new ("COMMAND [ARG...]");
   g_option_context_set_summary (context,
-                                "Accept IPC requests to create child "
+                                "Send IPC requests to create child "
                                 "processes.");
 
   g_option_context_add_main_entries (context, options, NULL);

bin/meson.build
@@ -66,6 +66,25 @@ executable(
 )
 
 executable(
+  'steam-runtime-launch-client',
+  sources: [
+    'launch-client.c',
+  ],
+  c_args : srt_c_args,
+  dependencies : [
+    threads,
+    gio_unix,
+    libglnx_dep,
+    libsteamrt_static_dep,
+  ],
+  # Needs pv_include_dirs for flatpak-*.h
+  include_directories : pv_include_dirs,
+  install : true,
+  build_rpath : bin_rpath,
+  install_rpath : bin_rpath,
+)
+
+executable(
   'steam-runtime-steam-remote',
   'steam-remote.c',
   c_args : srt_c_args,
@@ -92,6 +111,7 @@ if get_option('man')
     'check-requirements',
     'identify-library-abi',
     'input-monitor',
+    'launch-client',
     'steam-remote',
     'system-info',
     'urlopen',

debian/steam-runtime-tools-bin.install
@@ -1,4 +1,5 @@
 usr/bin/steam-runtime-check-requirements
 usr/bin/steam-runtime-input-monitor
+usr/bin/steam-runtime-launch-client
 usr/bin/steam-runtime-system-info
 usr/share/man/man1/steam-runtime-*

pressure-vessel/build-relocatable-install.py
@@ -101,11 +101,11 @@ SCRIPTS = [
 ]
 EXECUTABLES = [
     'pressure-vessel-adverb',
-    'pressure-vessel-launch',
     'pressure-vessel-launcher',
     'pressure-vessel-try-setlocale',
     'pressure-vessel-wrap',
     'pv-bwrap',
+    'steam-runtime-launch-client',
     'steam-runtime-system-info',
 ]
 

pressure-vessel/meson.build
@@ -125,26 +125,6 @@ executable(
 )
 
 executable(
-  'pressure-vessel-launch',
-  sources: [
-    'launch.c',
-  ],
-  c_args : pv_c_args,
-  dependencies : [
-    pressure_vessel_utils_dep,
-    threads,
-    gio_unix,
-    libglnx_dep,
-    json_glib,
-  ],
-  include_directories : pv_include_dirs,
-  install : true,
-  install_dir : pv_bindir,
-  build_rpath : pv_rpath,
-  install_rpath : pv_rpath,
-)
-
-executable(
   'pressure-vessel-launcher',
   sources: [
     'launcher.c',
@@ -289,7 +269,6 @@ endif
 if get_option('man')
   foreach tool : [
     'adverb',
-    'launch',
     'launcher',
     'locale-gen',
     'test-ui',

pressure-vessel/utils.h
@@ -29,10 +29,6 @@
 
 #include "missing.h"
 
-#define DBUS_NAME_DBUS "org.freedesktop.DBus"
-#define DBUS_INTERFACE_DBUS DBUS_NAME_DBUS
-#define DBUS_PATH_DBUS "/org/freedesktop/DBus"
-
 void pv_search_path_append (GString *search_path,
                             const gchar *item);
 

pressure-vessel/wrap-flatpak.c
@@ -32,7 +32,7 @@ get_subsandbox_adverb (const char *launch_executable)
   FlatpakBwrap *ret = flatpak_bwrap_new (flatpak_bwrap_empty_env);
 
   flatpak_bwrap_add_arg (ret, launch_executable);
-  /* Tell pressure-vessel-launch to send its whole environment
+  /* Tell steam-runtime-launch-client to send its whole environment
    * to the subsandbox, except for the parts that we edit later.
    * This effectively matches bwrap's behaviour. */
   flatpak_bwrap_add_arg (ret, "--pass-env-matching=*");
@@ -88,7 +88,7 @@ pv_wrap_check_flatpak (const char *tools_dir,
     g_info ("Running under Flatpak, version %s", flatpak_version);
 
   launch_executable = g_build_filename (tools_dir,
-                                        "pressure-vessel-launch",
+                                        "steam-runtime-launch-client",
                                         NULL);
 
   if (flatpak_version != NULL && strverscmp (flatpak_version, "1.11.0") >= 0)

commit 21ac90a06b4a2e54f1f2c5b57708fc295c2d6b41

Update comments/messages to refer to s-r-launch-client, not pv-launch
Fixes: 3fbb5d8a "Rename pv-launch to s-r-launch-client and move it to bin/"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/bwrap.c
@@ -716,7 +716,7 @@ static const char* unsecure_environment_variables[] = {
 
 /*
  * Populate @flatpak_subsandbox with environment variables from @container_env.
- * They'll be passed via pv-launch `--env`/`--unset-env`.
+ * They'll be passed via s-r-launch-client `--env`/`--unset-env`.
  */
 void
 pv_bwrap_container_env_to_subsandbox_argv (FlatpakBwrap *flatpak_subsandbox,

pressure-vessel/wrap.c
@@ -1080,8 +1080,8 @@ main (int argc,
       pv_environ_inherit_env (container_env, "XDG_RUNTIME_DIR");
 
       /* The bwrap envp will be completely ignored when calling
-       * pv-launch, and in fact putting them in its environment
-       * variables would be wrong, because pv-launch needs to see the
+       * s-r-launch-client, and in fact putting them in its environment
+       * variables would be wrong, because s-r-launch-client needs to see the
        * current execution environment's DBUS_SESSION_BUS_ADDRESS
        * (if different). For this reason we convert them to `--setenv`. */
       if (flatpak_subsandbox != NULL)
@@ -1096,7 +1096,7 @@ main (int argc,
    * We skip this if we are in a Flatpak environment, because in that case
    * we already used `--setenv` for all the variables that we care about and
    * the final_argv->envp will be ignored anyway, other than as a way to
-   * invoke pv-launch (for which original_environ is appropriate). */
+   * invoke s-r-launch-client (for which original_environ is appropriate). */
   if (!self->is_flatpak_env)
     {
       /* Note that these are two different FlatpakBwrap instances! */

tests/pressure-vessel/bwrap.c
@@ -216,7 +216,7 @@ test_from_container_env_subsandbox (Fixture *f,
   gsize i;
 
   pv_bwrap_container_env_to_subsandbox_argv (flatpak_subsandbox, f->container_env);
-  dump_bwrap (flatpak_subsandbox, "Arguments to add to pv-launch");
+  dump_bwrap (flatpak_subsandbox, "Arguments to add to s-r-launch-client");
   /*
    * Set variable => --env
    * Explicitly unset variable => --unset-env