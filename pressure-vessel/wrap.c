xytovl/steam-runtime-tools

commit ba4aaedeb45a7a741c6885aee75e2af66231d2cf

Rename pressure-vessel-launcher to s-r-launcher-service, move to bin/
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap.c
@@ -964,7 +964,7 @@ static GOptionEntry options[] =
     G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_launcher,
     "Instead of specifying a command with its arguments to execute, all the "
     "elements after '--' will be used as arguments for "
-    "'pressure-vessel-launcher'. All the environment variables that are "
+    "'steam-runtime-launcher-service'. All the environment variables that are "
     "edited by pressure-vessel, or that are known to be wrong in the new "
     "container, or that needs to inherit the value from the host system, "
     "will be locked. This option implies --batch.", NULL },
@@ -2427,11 +2427,11 @@ main (int argc,
     {
       g_autoptr(FlatpakBwrap) launcher_argv =
         flatpak_bwrap_new (flatpak_bwrap_empty_env);
-      g_autofree gchar *pressure_vessel_launcher = g_build_filename (tools_dir,
-                                                                     "pressure-vessel-launcher",
-                                                                     NULL);
-      g_debug ("Adding pressure-vessel-launcher '%s'...", pressure_vessel_launcher);
-      flatpak_bwrap_add_arg (launcher_argv, pressure_vessel_launcher);
+      g_autofree gchar *launcher_service = g_build_filename (tools_dir,
+                                                             "steam-runtime-launcher-service",
+                                                             NULL);
+      g_debug ("Adding steam-runtime-launcher-service '%s'...", launcher_service);
+      flatpak_bwrap_add_arg (launcher_argv, launcher_service);
 
       if (opt_verbose)
         flatpak_bwrap_add_arg (launcher_argv, "--verbose");

bin/launch-client.c
@@ -1118,7 +1118,7 @@ main (int argc,
 
       /* The host portal doesn't support options, so we always have to do
        * this the hard way. The subsandbox portal supports unset-env in
-       * versions >= 5. pressure-vessel-launcher always supports it. */
+       * versions >= 5. steam-runtime-launcher-service always supports it. */
       if (api == &launcher_api
           || (api == &subsandbox_api && get_portal_version () >= 5))
         {

bin/launcher-service.c
@@ -1,5 +1,5 @@
 /*
- * pressure-vessel-launcher — accept IPC requests to create child processes
+ * steam-runtime-launcher-service — accept IPC requests to create child processes
  *
  * Copyright © 2018 Red Hat, Inc.
  * Copyright © 2020 Collabora Ltd.
@@ -48,7 +48,6 @@
 #include "libglnx/libglnx.h"
 
 #include "flatpak-utils-base-private.h"
-#include "utils.h"
 
 /* Absence of GConnectFlags; slightly more readable than a magic number */
 #define CONNECT_FLAGS_NONE (0)
@@ -1079,7 +1078,7 @@ main (int argc,
 
   setlocale (LC_ALL, "");
 
-  g_set_prgname ("pressure-vessel-launcher");
+  g_set_prgname ("steam-runtime-launcher-service");
 
   /* Set up the initial base logging */
   _srt_util_set_glib_log_handler (FALSE);

bin/meson.build
@@ -85,6 +85,27 @@ executable(
 )
 
 executable(
+  'steam-runtime-launcher-service',
+  sources: [
+    'launcher-service.c',
+    '../pressure-vessel/flatpak-utils-base.c',
+  ],
+  c_args : srt_c_args,
+  dependencies : [
+    launcher_codegen_dep,
+    threads,
+    gio_unix,
+    libglnx_dep,
+    libsteamrt_static_dep,
+  ],
+  # Needs pv_include_dirs for flatpak-*.h
+  include_directories : pv_include_dirs,
+  install : true,
+  build_rpath : bin_rpath,
+  install_rpath : bin_rpath,
+)
+
+executable(
   'steam-runtime-steam-remote',
   'steam-remote.c',
   c_args : srt_c_args,
@@ -112,6 +133,7 @@ if get_option('man')
     'identify-library-abi',
     'input-monitor',
     'launch-client',
+    'launcher-service',
     'steam-remote',
     'system-info',
     'urlopen',

debian/steam-runtime-tools-bin.install
@@ -1,5 +1,6 @@
 usr/bin/steam-runtime-check-requirements
 usr/bin/steam-runtime-input-monitor
 usr/bin/steam-runtime-launch-client
+usr/bin/steam-runtime-launcher-service
 usr/bin/steam-runtime-system-info
 usr/share/man/man1/steam-runtime-*

pressure-vessel/build-relocatable-install.py
@@ -101,11 +101,11 @@ SCRIPTS = [
 ]
 EXECUTABLES = [
     'pressure-vessel-adverb',
-    'pressure-vessel-launcher',
     'pressure-vessel-try-setlocale',
     'pressure-vessel-wrap',
     'pv-bwrap',
     'steam-runtime-launch-client',
+    'steam-runtime-launcher-service',
     'steam-runtime-system-info',
 ]
 

pressure-vessel/meson.build
@@ -124,27 +124,6 @@ executable(
   install_rpath : pv_rpath,
 )
 
-executable(
-  'pressure-vessel-launcher',
-  sources: [
-    'launcher.c',
-  ],
-  c_args : pv_c_args,
-  dependencies : [
-    launcher_codegen_dep,
-    pressure_vessel_utils_dep,
-    threads,
-    gio_unix,
-    libglnx_dep,
-    json_glib,
-  ],
-  include_directories : pv_include_dirs,
-  install : true,
-  install_dir : pv_bindir,
-  build_rpath : pv_rpath,
-  install_rpath : pv_rpath,
-)
-
 pressure_vessel_wrap_lib = static_library(
   'pressure-vessel-wrap-lib',
   sources : [
@@ -267,7 +246,6 @@ endif
 if get_option('man')
   foreach tool : [
     'adverb',
-    'launcher',
     'locale-gen',
     'test-ui',
     'try-setlocale',

steam-runtime-tools/launcher-internal.h
@@ -1,6 +1,6 @@
 /*<private_header>*/
 /*
- * pressure-vessel-launcher — accept IPC requests to create child processes
+ * steam-runtime-launcher-service — accept IPC requests to create child processes
  *
  * Copyright © 2020 Collabora Ltd.
  *

commit 80b5028802599ed4b4fd8aa516e6efd11f46b171

Update a debug message to refer to s-r-launcher-service, not pv-launcher
Fixes: ba4aaede "Rename pressure-vessel-launcher to s-r-launcher-service, move to bin/"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap.c
@@ -394,7 +394,7 @@ main (int argc,
         }
 
       if (self->options.launcher)
-        g_debug ("Arguments for pv-launcher:");
+        g_debug ("Arguments for s-r-launcher-service:");
       else
         g_debug ("Wrapped command:");
 