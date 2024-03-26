5f4163c6c9d15ca4ca20fa061f3f7ceafcdfebcf
Simon McVittie <smcv@collabora.com>
2024-03-26 11:33:40 +0000

    file-lock: Factor out _srt_file_lock_acquire()
    No functional change intended.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- steam-runtime-tools/file-lock.c
+++ steam-runtime-tools/file-lock.c
@@ -40,60 +40,14 @@ struct _SrtFileLock
   gboolean is_ofd;
 };
 
-/**
- * srt_file_lock_new:
- * @at_fd: If not `AT_FDCWD` or -1, look up @path relative to this
- *  directory fd instead of the current working directory, as per `openat(2)`
- * @path: File to lock
- * @flags: Flags affecting how we lock the file
- * @error: Used to raise an error on failure
- *
- * Take out a lock on a file.
- *
- * If %SRT_FILE_LOCK_FLAGS_EXCLUSIVE is in @flags, the lock is an exclusive
- * (write) lock, which can be held by at most one process at a time. This is
- * appropriate when about to modify or delete the locked resource.
- * Otherwise it is a shared (read) lock, which excludes exclusive locks
- * but does not exclude other shared locks. This is appropriate when using
- * but not modifying the locked resource.
- *
- * If %SRT_FILE_LOCK_FLAGS_WAIT is not in @flags, raise %G_IO_ERROR_BUSY
- * if the lock cannot be obtained immediately.
- *
- * Returns: (nullable): A lock (release and free with srt_file_lock_free())
- *  or %NULL.
- */
-SrtFileLock *
-srt_file_lock_new (int at_fd,
-                   const gchar *path,
-                   SrtFileLockFlags flags,
-                   GError **error)
+static gboolean
+_srt_file_lock_acquire (int fd,
+                        const char *path,
+                        SrtFileLockFlags flags,
+                        gboolean *is_ofd_out,
+                        GError **error)
 {
-  glnx_autofd int fd = -1;
-  int open_flags = O_CLOEXEC | O_NOCTTY;
   int ofd;
-
-  g_return_val_if_fail (path != NULL, NULL);
-  g_return_val_if_fail (error == NULL || *error == NULL, NULL);
-  g_return_val_if_fail ((flags & SRT_FILE_LOCK_FLAGS_PROCESS_ORIENTED) == 0 ||
-                        (flags & SRT_FILE_LOCK_FLAGS_REQUIRE_OFD) == 0,
-                        NULL);
-
-  if (flags & SRT_FILE_LOCK_FLAGS_CREATE)
-    open_flags |= O_RDWR | O_CREAT;
-  else if (flags & SRT_FILE_LOCK_FLAGS_EXCLUSIVE)
-    open_flags |= O_RDWR;
-  else
-    open_flags |= O_RDONLY;
-
-  at_fd = glnx_dirfd_canonicalize (at_fd);
-  fd = TEMP_FAILURE_RETRY (openat (at_fd, path, open_flags, 0644));
-
-  if (fd < 0)
-    {
-      glnx_throw_errno_prefix (error, "openat(%s)", path);
-      return NULL;
-    }
 
   /* If PROCESS_ORIENTED, only do ofd == 0. If not, try 1 then 0. */
   for (ofd = (flags & SRT_FILE_LOCK_FLAGS_PROCESS_ORIENTED) ? 0 : 1;
@@ -145,7 +99,12 @@ srt_file_lock_new (int at_fd,
         }
 
       if (TEMP_FAILURE_RETRY (fcntl (fd, cmd, &l)) == 0)
-        break;
+        {
+          if (is_ofd_out != NULL)
+            *is_ofd_out = (ofd > 0);
+
+          return TRUE;
+        }
 
       saved_errno = errno;
 
@@ -161,15 +120,74 @@ srt_file_lock_new (int at_fd,
           g_set_error (error, G_IO_ERROR, G_IO_ERROR_BUSY,
                        "Unable to lock %s for %s: file is busy",
                        path, type_str);
-          return NULL;
+          return FALSE;
         }
 
-      glnx_throw_errno_prefix (error, "Unable to lock %s for %s",
-                               path, type_str);
+      return glnx_throw_errno_prefix (error, "Unable to lock %s for %s",
+                                      path, type_str);
+    }
+
+  g_return_val_if_reached (FALSE);
+}
+
+/**
+ * srt_file_lock_new:
+ * @at_fd: If not `AT_FDCWD` or -1, look up @path relative to this
+ *  directory fd instead of the current working directory, as per `openat(2)`
+ * @path: File to lock
+ * @flags: Flags affecting how we lock the file
+ * @error: Used to raise an error on failure
+ *
+ * Take out a lock on a file.
+ *
+ * If %SRT_FILE_LOCK_FLAGS_EXCLUSIVE is in @flags, the lock is an exclusive
+ * (write) lock, which can be held by at most one process at a time. This is
+ * appropriate when about to modify or delete the locked resource.
+ * Otherwise it is a shared (read) lock, which excludes exclusive locks
+ * but does not exclude other shared locks. This is appropriate when using
+ * but not modifying the locked resource.
+ *
+ * If %SRT_FILE_LOCK_FLAGS_WAIT is not in @flags, raise %G_IO_ERROR_BUSY
+ * if the lock cannot be obtained immediately.
+ *
+ * Returns: (nullable): A lock (release and free with srt_file_lock_free())
+ *  or %NULL.
+ */
+SrtFileLock *
+srt_file_lock_new (int at_fd,
+                   const gchar *path,
+                   SrtFileLockFlags flags,
+                   GError **error)
+{
+  glnx_autofd int fd = -1;
+  int open_flags = O_CLOEXEC | O_NOCTTY;
+  gboolean ofd;
+
+  g_return_val_if_fail (path != NULL, NULL);
+  g_return_val_if_fail (error == NULL || *error == NULL, NULL);
+  g_return_val_if_fail ((flags & SRT_FILE_LOCK_FLAGS_PROCESS_ORIENTED) == 0 ||
+                        (flags & SRT_FILE_LOCK_FLAGS_REQUIRE_OFD) == 0,
+                        NULL);
+
+  if (flags & SRT_FILE_LOCK_FLAGS_CREATE)
+    open_flags |= O_RDWR | O_CREAT;
+  else if (flags & SRT_FILE_LOCK_FLAGS_EXCLUSIVE)
+    open_flags |= O_RDWR;
+  else
+    open_flags |= O_RDONLY;
+
+  at_fd = glnx_dirfd_canonicalize (at_fd);
+  fd = TEMP_FAILURE_RETRY (openat (at_fd, path, open_flags, 0644));
+
+  if (fd < 0)
+    {
+      glnx_throw_errno_prefix (error, "openat(%s)", path);
       return NULL;
     }
 
-  g_assert (ofd == 0 || ofd == 1);
+  if (!_srt_file_lock_acquire (fd, path, flags, &ofd, error))
+    return NULL;
+
   return srt_file_lock_new_take (g_steal_fd (&fd), ofd);
 }
 
