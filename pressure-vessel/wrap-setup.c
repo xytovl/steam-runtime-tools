xytovl/steam-runtime-tools

commit 8d76938ef809823a7b11511146ffbdbb0bb66029

pv-wrap: Explicitly share $XDG_CONFIG_HOME, etc. if sharing $HOME
According to recent steamwebhelper issue reports, it's somewhat common
for ~/.config to be a symbolic link to some other filesystem, in which
case users will expect the target of the symlink to make it into the
container.

This commit only handles the case where an XDG environment variable is
explicitly set. The case where it is unset (defaulting to ~/.config,
etc.) was already handled in a previous commit.

It is normal for $XDG_DATA_DIRS to contain /usr/local/share:/usr/share,
and $XDG_CONFIG_DIRS might legitimately contain /usr paths too, so
downgrade the usual warning about paths below /usr to a debug message
for these particular variables.

steamrt/tasks#411

Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap-setup.c
@@ -1071,6 +1071,8 @@ typedef enum
   ENV_MOUNT_FLAGS_COLON_DELIMITED = (1 << 0),
   ENV_MOUNT_FLAGS_DEPRECATED = (1 << 1),
   ENV_MOUNT_FLAGS_READ_ONLY = (1 << 2),
+  ENV_MOUNT_FLAGS_IF_HOME_SHARED = (1 << 3),
+  ENV_MOUNT_FLAGS_USR_QUIET = (1 << 4),
   ENV_MOUNT_FLAGS_NONE = 0
 } EnvMountFlags;
 
@@ -1100,11 +1102,24 @@ static const EnvMount known_required_env[] =
     { "STEAM_COMPAT_TOOL_PATH", ENV_MOUNT_FLAGS_DEPRECATED },
     { "STEAM_COMPAT_TOOL_PATHS", ENV_MOUNT_FLAGS_COLON_DELIMITED },
     { "STEAM_EXTRA_COMPAT_TOOLS_PATHS", ENV_MOUNT_FLAGS_COLON_DELIMITED },
+    { "XDG_CACHE_HOME", ENV_MOUNT_FLAGS_IF_HOME_SHARED },
+    { "XDG_CONFIG_HOME", ENV_MOUNT_FLAGS_IF_HOME_SHARED },
+    { "XDG_CONFIG_DIRS",
+      ENV_MOUNT_FLAGS_IF_HOME_SHARED
+      | ENV_MOUNT_FLAGS_COLON_DELIMITED
+      | ENV_MOUNT_FLAGS_USR_QUIET },
+    { "XDG_DATA_HOME", ENV_MOUNT_FLAGS_IF_HOME_SHARED },
+    { "XDG_DATA_DIRS",
+      ENV_MOUNT_FLAGS_IF_HOME_SHARED
+      | ENV_MOUNT_FLAGS_COLON_DELIMITED
+      | ENV_MOUNT_FLAGS_USR_QUIET },
+    { "XDG_STATE_HOME", ENV_MOUNT_FLAGS_IF_HOME_SHARED },
 };
 
 static void
 bind_and_propagate_from_environ (SrtSysroot *sysroot,
                                  const char * const *current_env,
+                                 PvHomeMode home_mode,
                                  FlatpakExports *exports,
                                  PvEnviron *container_env,
                                  const char *variable,
@@ -1120,6 +1135,10 @@ bind_and_propagate_from_environ (SrtSysroot *sysroot,
 
   g_return_if_fail (exports != NULL);
   g_return_if_fail (variable != NULL);
+
+  if (home_mode != PV_HOME_MODE_SHARED
+      && (flags & ENV_MOUNT_FLAGS_IF_HOME_SHARED))
+    return;
 
   value = _srt_environ_getenv (current_env, variable);
 
@@ -1172,7 +1191,15 @@ bind_and_propagate_from_environ (SrtSysroot *sysroot,
         }
 
       if (flatpak_has_path_prefix (canon, "/usr"))
-        g_warning_once ("Binding directories that are located under \"/usr/\" is not supported!");
+        {
+          if (!(flags & ENV_MOUNT_FLAGS_USR_QUIET))
+            g_warning_once ("Binding directories that are located under \"/usr/\" is not supported!");
+          else
+            g_debug ("Ignoring %s=\"%s%s%s\" because it is below /usr",
+                     variable, before, values[i], after);
+
+          continue;
+        }
 
       g_info ("Bind-mounting %s=\"%s%s%s\" from the current env as %s=\"%s%s%s\" in the host",
               variable, before, values[i], after,
@@ -1204,6 +1231,7 @@ bind_and_propagate_from_environ (SrtSysroot *sysroot,
 void
 pv_bind_and_propagate_from_environ (SrtSysroot *sysroot,
                                     const char * const *current_env,
+                                    PvHomeMode home_mode,
                                     FlatpakExports *exports,
                                     PvEnviron *container_env)
 {
@@ -1219,7 +1247,7 @@ pv_bind_and_propagate_from_environ (SrtSysroot *sysroot,
         {
           /* If we're using bubblewrap directly, we can and must make
            * sure that all required directories are bind-mounted */
-          bind_and_propagate_from_environ (sysroot, current_env,
+          bind_and_propagate_from_environ (sysroot, current_env, home_mode,
                                            exports, container_env,
                                            known_required_env[i].name,
                                            known_required_env[i].flags);
@@ -1229,6 +1257,7 @@ pv_bind_and_propagate_from_environ (SrtSysroot *sysroot,
           /* If we're using a Flatpak subsandbox, we have no choice but to
            * rely on the fact that any directory available to the parent app
            * is also going to be available to the subsandbox */
+          g_return_if_fail (home_mode == PV_HOME_MODE_SHARED);
           pv_environ_setenv (container_env,
                              known_required_env[i].name,
                              _srt_environ_getenv (current_env,

tests/pressure-vessel/wrap-setup.c
@@ -1352,6 +1352,7 @@ test_use_home_shared (Fixture *f,
   was_fatal = g_log_set_always_fatal (G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL);
   pv_bind_and_propagate_from_environ (f->mock_host,
                                       mock_environ,
+                                      PV_HOME_MODE_SHARED,
                                       env_exports,
                                       container_env);
   g_log_set_always_fatal (was_fatal);

pressure-vessel/wrap-setup.h
@@ -32,6 +32,7 @@
 #include "flatpak-exports-private.h"
 #include "runtime.h"
 #include "wrap-discord.h"
+#include "wrap-home.h"
 #include "wrap-pipewire.h"
 
 gchar *pv_wrap_check_bwrap (const char *pkglibexecdir,
@@ -95,5 +96,6 @@ void pv_share_temp_dir (FlatpakExports *exports,
                         PvEnviron *container_env);
 void pv_bind_and_propagate_from_environ (SrtSysroot *sysroot,
                                          const char * const *current_env,
+                                         PvHomeMode home_mode,
                                          FlatpakExports *exports,
                                          PvEnviron *container_env);

pressure-vessel/wrap.c
@@ -1654,6 +1654,7 @@ main (int argc,
 
   pv_bind_and_propagate_from_environ (real_root,
                                       _srt_const_strv (original_environ),
+                                      home_mode,
                                       exports, container_env);
 
   if (flatpak_subsandbox == NULL)

commit 0b6ccd23b2addbc32e348fae44d7ed9a8b922006

wrap-setup: Don't revert environment variables we already changed
Now that pv-runtime and pv_bind_and_propagate_from_environ() both want
to modify XDG_DATA_DIRS, they'll fight: pv-runtime alters XDG_DATA_DIRS,
and then pv_bind_and_propagate_from_environ() alters it back again.

For Flatpak users, this manifested as an inability to load some (all?)
Vulkan layers in the pressure-vessel sub-sandbox, because
/usr/lib/pressure-vessel/overrides/share is prepended to the search path
but then discarded. Non-Flatpak users will not usually be affected,
although in principle they might be if their XDG_DATA_DIRS contains
non-canonical paths like `/opt/lib/../share`.

This fixes a regression in 0.20240301.0.

Resolves: https://github.com/ValveSoftware/steam-runtime/issues/662
Fixes: 8d76938e "pv-wrap: Explicitly share $XDG_CONFIG_HOME, etc. if sharing $HOME"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap-setup.c
@@ -1143,7 +1143,10 @@ bind_and_propagate_from_environ (SrtSysroot *sysroot,
       && (flags & ENV_MOUNT_FLAGS_IF_HOME_SHARED))
     return;
 
-  value = _srt_environ_getenv (current_env, variable);
+  if (_srt_env_overlay_contains (container_env, variable))
+    value = _srt_env_overlay_get (container_env, variable);
+  else
+    value = _srt_environ_getenv (current_env, variable);
 
   if (value == NULL)
     return;
@@ -1246,13 +1249,15 @@ pv_bind_and_propagate_from_environ (SrtSysroot *sysroot,
 
   for (i = 0; i < G_N_ELEMENTS (known_required_env); i++)
     {
+      const char *name = known_required_env[i].name;
+
       if (exports != NULL)
         {
           /* If we're using bubblewrap directly, we can and must make
            * sure that all required directories are bind-mounted */
           bind_and_propagate_from_environ (sysroot, current_env, home_mode,
                                            exports, container_env,
-                                           known_required_env[i].name,
+                                           name,
                                            known_required_env[i].flags);
         }
       else
@@ -1261,10 +1266,10 @@ pv_bind_and_propagate_from_environ (SrtSysroot *sysroot,
            * rely on the fact that any directory available to the parent app
            * is also going to be available to the subsandbox */
           g_return_if_fail (home_mode == PV_HOME_MODE_SHARED);
-          _srt_env_overlay_set (container_env,
-                                known_required_env[i].name,
-                                _srt_environ_getenv (current_env,
-                                                     known_required_env[i].name));
+
+          if (!_srt_env_overlay_contains (container_env, name))
+            _srt_env_overlay_set (container_env, name,
+                                  _srt_environ_getenv (current_env, name));
         }
     }
 }

steam-runtime-tools/env-overlay-internal.h
@@ -79,6 +79,8 @@ gboolean _srt_env_overlay_pass_matching_pattern_cli (SrtEnvOverlay *self,
                                                      GError **error);
 
 GList *_srt_env_overlay_get_vars (SrtEnvOverlay *self);
+gboolean _srt_env_overlay_contains (SrtEnvOverlay *self,
+                                    const char *var);
 const char *_srt_env_overlay_get (SrtEnvOverlay *self,
                                   const char *var);
 

steam-runtime-tools/env-overlay.c
@@ -248,6 +248,23 @@ _srt_env_overlay_get_vars (SrtEnvOverlay *self)
 }
 
 /*
+ * _srt_env_overlay_contains:
+ * @self: Environment variables to set or unset
+ * @var: (type filename): An environment variable name
+ *
+ * Returns: %TRUE if @var is either set or forced to be unset,
+ *  or %FALSE if it is inherited
+ */
+gboolean
+_srt_env_overlay_contains (SrtEnvOverlay *self,
+                           const char *var)
+{
+  g_return_val_if_fail (self != NULL, FALSE);
+
+  return g_hash_table_lookup_extended (self->values, var, NULL, NULL);
+}
+
+/*
  * _srt_env_overlay_get:
  * @self: Environment variables to set or unset
  * @var: (type filename): An environment variable name